055ba487f405081142645ea8c1cbfddd1aba5e41
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2021-11-21 21:22:08 -0800

    source code formatting

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use std::collections::VecDeque;
-use std::num::Wrapping;
 use std::collections::HashMap;
+use std::collections::VecDeque;
 use std::io::{self, Write};
+use std::num::Wrapping;
 
-type CellData = Wrapping::<u8>;
+type CellData = Wrapping<u8>;
 
 macro_rules! wrap_data {
-    ($arg:expr) => { Wrapping::<u8>($arg) }
+    ($arg:expr) => {
+        Wrapping::<u8>($arg)
+    };
 }
 
 const VAL_ZERO: CellData = wrap_data!(0);
@@ -33,7 +35,7 @@ macro_rules! debugln {
 struct Jump {
     here: isize, // location/index of instruction
     jump_to: isize, // location/index to conditionally jump to
-    // positive numbers indicate start of loops, negative numbers indicate end of loops
+                 // positive numbers indicate start of loops, negative numbers indicate end of loops
 }
 
 #[derive(Debug)]
@@ -47,20 +49,17 @@ enum Op {
     Print,
 }
 
-
-
 fn main() {
     //let program_str: &str = "+++[>++<-]++"; [[[]][]][]
-    let _pg_hello_world = ">>>>>+[-->-[>>+ >-----<<]< --<---]>-.>>>+.>>..+++[.>]<<<<.+++.------.<<-.>>>>+.";
+    let _pg_hello_world =
+        ">>>>>+[-->-[>>+ >-----<<]< --<---]>-.>>>+.>>..+++[.>]<<<<.+++.------.<<-.>>>>+.";
     let _pg_cat = ",+[-.,+]";
-    let program_str = _pg_hello_world;
     let input_str = "cool cat :3";
-
-
+    let program_str = _pg_hello_world;
 
     let mut program = Vec::<Op>::new();
     let mut jump_index: isize = 1;
-    let mut jump_stack= Vec::new();
+    let mut jump_stack = Vec::new();
     // TODO: explicitly check unmatched []
     for c in program_str.chars() {
         match c {
@@ -71,39 +70,42 @@ fn main() {
             '.' => program.push(Op::Print),
             ',' => program.push(Op::Input),
             '[' => {
-                program.push(Op::JumpIfZero(Jump{here: jump_index, jump_to: -jump_index}));
+                program.push(Op::JumpIfZero(Jump {
+                    here: jump_index,
+                    jump_to: -jump_index,
+                }));
                 jump_stack.push(jump_index);
                 jump_index += 1;
-            },
-            ']' => {
-                match jump_stack.pop() {
-                    Some(jump_to) => {
-                        program.push(Op::JumpIfNonZero(Jump{here: -jump_to, jump_to: jump_to}));
-                    }
-                    None => {
-                        panic!("Unmatched closing bracket");
-                    }
+            }
+            ']' => match jump_stack.pop() {
+                Some(jump_to) => {
+                    program.push(Op::JumpIfNonZero(Jump {
+                        here: -jump_to,
+                        jump_to: jump_to,
+                    }));
+                }
+                None => {
+                    panic!("Unmatched closing bracket");
                 }
             },
 
-            _ => () // all other characters are treated as comments
+            _ => (), // all other characters are treated as comments
         }
     }
 
     let mut jump_map = HashMap::new();
     for (i, op) in program.iter().enumerate() {
         match op {
-            Op::JumpIfZero(Jump{here, jump_to: _}) |
-            Op::JumpIfNonZero(Jump{here, jump_to: _}) => {
+            Op::JumpIfZero(Jump { here, jump_to: _ })
+            | Op::JumpIfNonZero(Jump { here, jump_to: _ }) => {
                 jump_map.insert(here, i);
-            },
-            _ => ()
+            }
+            _ => (),
         }
     }
 
     debugln!("{:?}", program);
     debugln!("{:?}", jump_map);
-
 
     let mut input = VecDeque::<CellData>::new();
     for c in input_str.chars() {
@@ -130,27 +132,25 @@ fn main() {
                 while ptr >= tape.len() {
                     tape.push(VAL_ZERO);
                 }
-            },
+            }
             Op::Print => {
                 print!("{}", tape[ptr].0 as char);
                 io::stdout().flush().unwrap();
+            }
+            Op::Input => match input.pop_front() {
+                Some(v) => tape[ptr] = v,
+                None => tape[ptr] = VAL_MINUS_ONE,
             },
-            Op::Input => {
-                match input.pop_front() {
-                    Some(v) => tape[ptr] = v,
-                    None => tape[ptr] = VAL_MINUS_ONE
-                }
-            },
-            Op::JumpIfZero(Jump{here: _, jump_to}) => {
+            Op::JumpIfZero(Jump { here: _, jump_to }) => {
                 if tape[ptr] == VAL_ZERO {
                     ip = jump_map[&jump_to];
                 }
-            },
-            Op::JumpIfNonZero(Jump{here: _, jump_to}) => {
+            }
+            Op::JumpIfNonZero(Jump { here: _, jump_to }) => {
                 if tape[ptr] != VAL_ZERO {
                     ip = jump_map[&jump_to];
                 }
-            },
+            }
             //_ => println!("wtf"),
         }
         debugln!("{:?} {:?}", program[ip], tape);
@@ -161,5 +161,4 @@ fn main() {
             break;
         }
     }
-
 }
